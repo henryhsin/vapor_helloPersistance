3ea558e7768bbb72423950513dabe5a05e11c69b
HenryHsin <hooy123456@gmail.com>
2017-04-09 14:00:20 +0800

    First commit

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import Vapor
+
+final class Acronym: Model{
+    var short: String
+    var long: String
+    
+    
+    var id: Node?
+    var exists = false
+    
+    
+    init(short: String, long: String){
+        self.id = nil
+        self.short = short
+        self.long = long
+    }
+    
+    init(node: Node, in context: Context) throws {
+        id = try node.extract("id")
+        short = try node.extract("short")
+        long = try node.extract("long")
+    }
+    
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node: [
+            "short": short,
+            "long": long
+            ])
+    }
+    
+    static func prepare(_ database: Database) throws {
+        try database.create("acronyms"){ (users) in
+            users.id()
+            users.string("short")
+            users.string("long")
+            
+        }
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete("acronyms")
+    }
+}
+
+
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,62 @@
 import Vapor
+import VaporPostgreSQL
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+try drop.addProvider(VaporPostgreSQL.Provider.self)
+drop.preparations.append(Acronym.self)
+
+
+
+drop.get("version"){request in
+    if let db = drop.database?.driver as? PostgreSQLDriver{
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    }else{
+        return "No db connection."
+    }
 }
 
-drop.resource("posts", PostController())
+drop.get("model"){request in
+    let acronym = Acronym(short: "AFK", long: "Away From Keyboard~~")
+    return try acronym.makeJSON()
+    
+}
 
+drop.get("test"){request in
+    var acronym = Acronym(short: "FUM", long: "Fuck Your Mother!!")
+    try acronym.save()
+    return try JSON(node: Acronym.all().makeNode())
+    
+}
+
+drop.post("new"){request in
+    var acronym = try Acronym(node: request.json)
+    print(request)
+    try acronym.save()
+    return acronym
+}
+
+drop.get("all"){request in
+    return try JSON(node: Acronym.all().makeNode())
+    
+}
+
+drop.get("first"){request in
+    return try JSON(node: Acronym.query().first()?.makeNode())
+}
+
+drop.get("ASAP"){request in
+    return try JSON(node: Acronym.query().filter("short", "ASAP").all().makeNode())
+}
+
+drop.get("nonASAP"){request in
+    return try JSON(node: Acronym.query().filter("short", .notEquals , "ASAP") .all().makeNode())
+}
+
+drop.get("delete-ASAP"){request in
+    let query = try Acronym.query().filter("short", "ASAP")
+    try query.delete()
+    return try JSON(node: Acronym.all().makeNode())
+}
 drop.run()
